defd784897494cc39e37bc00cf6a6f051407951e
muhammadyoussef <muhammadyoussef.27@gmail.com>
2020-01-10 17:23:18 +0200

    add DI scopes

---
+++ app/src/main/java/me/muhammadyoussef/weatherio/di/scope/ActivityScope.java
@@ -0,0 +1,11 @@
+package me.muhammadyoussef.weatherio.di.scope;
+
+import javax.inject.Scope;
+
+/**
+ * This annotation identifies an Activity-level scope to be used for dependencies injection.
+ */
+
+@Scope
+public @interface ActivityScope {
+}
