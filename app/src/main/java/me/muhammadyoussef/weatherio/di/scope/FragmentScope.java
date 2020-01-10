ccaabcd151fb607810bdca62fdde8fdfbb69d9c4
muhammadyoussef <muhammadyoussef.27@gmail.com>
2020-01-10 17:23:18 +0200

    add DI scopes

---
+++ app/src/main/java/me/muhammadyoussef/weatherio/di/scope/FragmentScope.java
@@ -0,0 +1,11 @@
+package me.muhammadyoussef.weatherio.di.scope;
+
+import javax.inject.Scope;
+
+/**
+ * This annotation identifies an Fragment-level scope to be used for dependencies injection.
+ */
+
+@Scope
+public @interface FragmentScope {
+}
