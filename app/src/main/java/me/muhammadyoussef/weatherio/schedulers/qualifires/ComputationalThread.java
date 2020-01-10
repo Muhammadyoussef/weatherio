3632369384816b61a546d2eb5d5dc667290f139b
muhammadyoussef <muhammadyoussef.27@gmail.com>
2020-01-10 17:34:28 +0200

    add schedulers

---
+++ app/src/main/java/me/muhammadyoussef/weatherio/schedulers/qualifires/ComputationalThread.java
@@ -0,0 +1,13 @@
+package me.muhammadyoussef.weatherio.schedulers.qualifires;
+
+import com.rxmuhammadyoussef.core.schedulers.ThreadSchedulers;
+
+import javax.inject.Qualifier;
+
+/**
+ This qualifier is used for distinguishing between different {@link ThreadSchedulers} for dependency injection.
+ */
+
+@Qualifier
+public @interface ComputationalThread {
+}
